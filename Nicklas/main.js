3dc721e83b35b435870158bde30f6ae67795b3d8
NicklasParmer <nicsi16@student.sdu.dk>
2021-03-16 14:12:20 +0100

    copy

---
+++ Nicklas/main.js
@@ -0,0 +1,29 @@
+class main {
+
+    constructor() {
+        this.world = [new worldCreatorClass()];
+        this.game = this.game();
+    }
+    
+    
+
+    game(){
+        let game
+        return game;
+
+    }
+
+    inventory(){
+        let items = [];
+        return items;
+    }
+
+    addItem(theItem){
+        this.inventory.items.push(theItem);
+    }
+
+    removeItem(){
+
+    }
+   
+} 
