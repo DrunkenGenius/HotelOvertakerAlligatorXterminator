fdb703c3f8bef1166df7e4d83e46a90d5cbecaad
Jakobmost <32058527+Jakobmost@users.noreply.github.com>
2021-03-23 13:12:45 +0100

    asdsfgasdg

--- Jakob/main.js
+++ Jakob/main.js
@@ -1,5 +1,5 @@
-let world = [new worldCreatorClass()];
-
+let world = new worldCreatorClass();
+console.log(world.rooms[0]);
 let player1 = new playerClass;
 //console.log(player1);
 
@@ -8,51 +8,33 @@ player1.addItem(wep1);
 
 //___________________________________________
 
+function printText(yourText){
+    for (let i = 0; i < yourText.length(); i++){
+
+    }
+}
+
 window.addEventListener('load', function () {
-
-    let player = new playerClass;
-    let hotel = new roomClass;
-
-    roomDesc = {
-        roomNameA: "RoomeA", items: ["chair", "Sword"],
-        roomNameB: "RoomeB", items: ["chair", "Sword"]
-     }
-
+  
     const status = document.getElementById('status');
-    status.innerHTML = 'Your status is now: ' + player.hp;
-
+    status.innerHTML = `Your status.. Player hp: 
+    ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
+   
     const enterBtn = document.getElementById('enterBtn');
     const userInput = document.getElementById('userInput');
 
     enterBtn.addEventListener('click', function () {
-            status.innerHTML = 'your status is now: ' + player.hp;
+            status.innerHTML = `Your status.. Player hp: 
+            ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
 
             switch (userInput.value.toUpperCase() ){
                     case 'SEARCH': {
-                        let dice = Math.floor(Math.random() * 6) + 1;
-                        if (dice <= 3) {
-                            status.innerHTML += '<br/>you found something...';
-                            dice = Math.floor(Math.random() * 6) + 1;
-                            if (dice == 1) {
-                                status.innerHTML += '<br/>... a rosted chicken!';
-                                state.hp += Math.floor(Math.random() * 6) + 1;
-                            }
-                            if (dice == 2) {
-                                status.innerHTML += '<br/>... some coins!';
-                                state.score += Math.floor(Math.random() * 6) + 1;
-                                state.xp += Math.floor(Math.random() * 6) + 1;
-                            }
-                            if (dice == 3) {
-                                status.innerHTML += '<br/>... poisonous mushroom!';
-                                state.hp -= Math.floor(Math.random() * 6) + 1;
-                                state.xp += 1;
-                            }
-                        } else {
-                            status.innerHTML += '<br/>you found nothing.';
-                        }
+                        
                     }
                     break;
-                    case 'KILL': {
+
+                    case 'ATTACK 0' : {
+                        status.innerHTML += '<br/>You slowly walk towards your enemy.';
                         let dice = Math.floor(Math.random() * 6) + 1;
                         if (dice <= 2) {
                             status.innerHTML += '<br/>you killed a monster!';
@@ -64,6 +46,35 @@ window.addEventListener('load', function () {
                         }
                     }
                     break;
+
+                    case 'ATTACK 1': {
+                        let dice = Math.floor(Math.random() * 6) + 1;
+                        if (dice <= 2) {
+                            status.innerHTML += '<br/>you killed a monster!';
+                            state.hp -= Math.floor(Math.random() * 6) + 1;
+                            state.xp += Math.floor(Math.random() * 6) + 1;
+                            state.score += Math.floor(Math.random() * 2);
+                        } else {
+                            status.innerHTML += '<br/>you tried to kill a monster... no luck!';
+                        }
+                    }
+                    break;
+
+                    case 'LOOT':{
+
+                    }
+                    break;
+
+                    case 'FLEE':{
+
+                    }
+                    break;
+
+                    
+                    case 'GOTO':{
+
+                    }
+
                     default: {
                         status.innerHTML += '<br/>...command not recognized...';
                     }
