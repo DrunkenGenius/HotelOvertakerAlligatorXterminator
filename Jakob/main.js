87da0891f13681e831be2800fc303b62e8ee0394
Jakobmost <32058527+Jakobmost@users.noreply.github.com>
2021-03-23 12:24:45 +0100

    PUSHMEEE

--- Jakob/main.js
+++ Jakob/main.js
@@ -1,26 +1,8 @@
+let world = [new worldCreatorClass()];
 
-    let world = [new worldCreatorClass()];
-    let items = [];
-    
-    function inventory(){
-        return items;
-    }
+let player1 = new playerClass;
+//console.log(player1);
 
-    function addItem(theItem){
-        items.push(theItem);
-    }
-
-    function removeItem(theItem){
-        items.splice(theItem);
-    }
-   
- 
 let wep1 = new weaponClass;
-console.log(wep1);
-addItem(wep1);
-let food1 = new foodClass;
-addItem(food1);
-console.log(items);
-removeItem(0,0);
-console.log(items);
+player1.addItem(wep1);
 
