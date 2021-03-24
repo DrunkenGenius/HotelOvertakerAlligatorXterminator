14784acecc7d576607cb307fd344f38ecc9fbff4
NicklasParmer <nicsi16@student.sdu.dk>
2021-03-24 21:05:11 +0100

    a

--- Nicklas/main.js
+++ Nicklas/main.js
@@ -14,40 +14,41 @@ window.addEventListener('load', function () {
     const status = document.getElementById('status');
     status.innerHTML = `Your status.. Player hp: ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
 
-    
+
 
     const enterBtn = document.getElementById('enterBtn');
     const userInput = document.getElementById('userInput');
 
     enterBtn.addEventListener('click', function () {
-            status.innerHTML = `Your status.. Player hp: ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
+        status.innerHTML = `Your status.. Player hp: ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
 
-            switch (userInput.value.toUpperCase() ){
-                    case 'SEARCH': {
+        switch (userInput.value.toUpperCase()) {
+            case 'SEARCH': {
+                for (let index = 0; index < world.rooms[player1.location].enemies.length; index++) {
+                    
+                }
 
-                        if (player1.location == 0){
-                            status.innerHTML += 
-                            '<br/>You are in room:           ' + world.rooms[0].name + 
+                    if (player1.location == 0) {
+                        status.innerHTML +=
+                            '<br/>You are in room:           ' + world.rooms[0].name +
                             '<br/>The room to your left is:  ' + world.rooms[2].name +
                             '<br/>The room to your right is: ' + world.rooms[1].name;
-                            //'<br/>' + world.rooms[player1.location].enemies.length + ' hungry ememies are looking at you!';
-                        } else if (player1.location == 1){
-                            status.innerHTML += 
-                            '<br/>You are in room:           ' + world.rooms[1].name + 
+                        //'<br/>' + world.rooms[player1.location].enemies.length + ' hungry ememies are looking at you!';
+                    } else if (player1.location == 1) {
+                        status.innerHTML +=
+                            '<br/>You are in room:           ' + world.rooms[1].name +
                             '<br/>The room to your left is:  ' + world.rooms[0].name +
                             '<br/>The room to your right is: ' + world.rooms[2].name;
-                        } else{
-                            status.innerHTML += 
-                            '<br/>You are in room:           ' + world.rooms[2].name + 
+                    } else {
+                        status.innerHTML +=
+                            '<br/>You are in room:           ' + world.rooms[2].name +
                             '<br/>The room to your left is:  ' + world.rooms[1].name +
                             '<br/>The room to your right is: ' + world.rooms[0].name;
-                        }
-                        
-                         
-
                     }
-                    break;
-                        /* let dice = Math.floor(Math.random() * 6) + 1;
+                
+            }
+            break;
+            /* let dice = Math.floor(Math.random() * 6) + 1;
                         if (dice <= 3) {
                             status.innerHTML += '<br/>you found something...';
                             dice = Math.floor(Math.random() * 6) + 1;
@@ -69,21 +70,21 @@ window.addEventListener('load', function () {
                         }
                     }
                     break; */
-                    case 'KILL': {
-                        let dice = Math.floor(Math.random() * 6) + 1;
-                        if (dice <= 2) {
-                            status.innerHTML += '<br/>you killed a monster!';
-                            player1.hp -= Math.floor(Math.random() * 6) + 1;
-                            player1.xp += Math.floor(Math.random() * 6) + 1;
-                        } else {
-                            status.innerHTML += '<br/>you tried to kill a monster... no luck!';
-                        }
-                    }
-                    break;
-                    default: {
-                        status.innerHTML += '<br/>...command not recognized...';
-                    }
-                }
-            });
-
+        case 'KILL': {
+            let dice = Math.floor(Math.random() * 6) + 1;
+            if (dice <= 2) {
+                status.innerHTML += '<br/>you killed a monster!';
+                player1.hp -= Math.floor(Math.random() * 6) + 1;
+                player1.xp += Math.floor(Math.random() * 6) + 1;
+            } else {
+                status.innerHTML += '<br/>you tried to kill a monster... no luck!';
+            }
+        }
+        break;
+        default: {
+            status.innerHTML += '<br/>...command not recognized...';
+        }
+        }
     });
+
+});
