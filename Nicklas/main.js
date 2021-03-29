a63af887aea38c6a862438ab029e6e5243e69931
NicklasParmer <nicsi16@student.sdu.dk>
2021-03-29 13:13:04 +0200

    html

--- Nicklas/main.js
+++ Nicklas/main.js
@@ -11,17 +11,20 @@ player1.addItem(wep1);
 
 window.addEventListener('load', function () {
 
-    const status = document.getElementById('status');
-    status.innerHTML = `Your status.. Player hp: ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
+    const status = document.getElementById('playerStats');
+    status.innerHTML = `Player hp: ${player1.hp} </br> Level: ${player1.level} </br> xp: ${player1.xp}/1 `;
 
+    const items = document.getElementById('playerItems')
+    items.innerHTML = `Player Weapon Equipped: ${player1.weapon.name}`;
 
 
     const enterBtn = document.getElementById('enterBtn');
     const userInput = document.getElementById('userInput');
 
     enterBtn.addEventListener('click', function () {
-        status.innerHTML = `Your status.. Player hp: ${player1.hp}, xp: ${player1.xp} level ${player1.level}`;
-
+        const gamePlay = document.getElementById('gamePlay');
+        gamePlay.innerHTML = `You try to ${userInput.value}: `;
+        
         switch (userInput.value.toUpperCase()) {
             case 'SEARCH': {
                 for (let index = 0; index < world.rooms[player1.location].enemies.length; index++) {
@@ -29,18 +32,18 @@ window.addEventListener('load', function () {
                 }
 
                     if (player1.location == 0) {
-                        status.innerHTML +=
+                        gamePlay.innerHTML +=
                             '<br/>You are in room:           ' + world.rooms[0].name +
                             '<br/>The room to your left is:  ' + world.rooms[2].name +
                             '<br/>The room to your right is: ' + world.rooms[1].name;
                         //'<br/>' + world.rooms[player1.location].enemies.length + ' hungry ememies are looking at you!';
                     } else if (player1.location == 1) {
-                        status.innerHTML +=
+                        gamePlay.innerHTML +=
                             '<br/>You are in room:           ' + world.rooms[1].name +
                             '<br/>The room to your left is:  ' + world.rooms[0].name +
                             '<br/>The room to your right is: ' + world.rooms[2].name;
                     } else {
-                        status.innerHTML +=
+                        gamePlay.innerHTML +=
                             '<br/>You are in room:           ' + world.rooms[2].name +
                             '<br/>The room to your left is:  ' + world.rooms[1].name +
                             '<br/>The room to your right is: ' + world.rooms[0].name;
